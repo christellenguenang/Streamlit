271d0d1caf021e744f77e1e41dc9f141b55e933e
christellenguenang <108927688+christellenguenang@users.noreply.github.com>
2023-07-14 12:23:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -19,11 +19,27 @@ st.markdown(" ## I. Les Dashboards")
 
 # Importation des bases achats, clics et impression
 
-response = requests.get("http://127.0.0.1:8000/fusion", verify=False, timeout=30)
-fusion= response.json()
-fusion = pd.DataFrame(fusion)
-if __name__=='__API_NGUENANG__':
- uvicorn.run(app, host='127.0.0.1',port=8000)
+#response = requests.get("http://127.0.0.1:8000/fusion", verify=False, timeout=30)
+#fusion= response.json()
+#fusion = pd.DataFrame(fusion)
+#if __name__=='__API_NGUENANG__':
+ #uvicorn.run(app, host='127.0.0.1',port=8000)
+
+achats = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/achats.csv")
+clics = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/clics.csv")
+impressions = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/impressions.csv")
+
+    # On fusionne les 3 bases
+fusion_1 = pd.merge(clics, impressions, on="cookie_id")
+fusion = pd.merge(fusion_1, achats, on="cookie_id")
+    # changement du type des variables de temps
+
+fusion['timestamp_x'] = pd.to_datetime(fusion['timestamp_x'], unit='s')
+fusion['date_impressions'] = fusion['timestamp_x'].dt.strftime('01-01-1970 %H:%M:%S')
+fusion['timestamp_y'] = pd.to_datetime(fusion['timestamp_y'], unit='s')
+fusion['date_clics'] = fusion['timestamp_y'].dt.strftime('01-01-1970 %H:%M:%S')
+fusion['timestamp'] = pd.to_datetime(fusion['timestamp'], unit='s')
+fusion['date_achats'] = fusion['timestamp'].dt.strftime('01-01-1970 %H:%M:%S')
 
 # Création des filtres
 
