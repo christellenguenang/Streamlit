0e9d3cccfd76f4d6c52a602df096114be7d8b501
christellenguenang <108927688+christellenguenang@users.noreply.github.com>
2023-07-14 15:19:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,23 @@ import numpy as np
 st.title("Bienvenue sur mon application!")
 st.markdown(" ## I. Les Dashboards")
 
+app = Flask(__name__)
 
+@app.route("/fusion")
+def get_donnees():
+    # Importation des bases achats, clics, et impressions
+    achats = pd.read_csv("achats.csv")
+    clics = pd.read_csv("clics.csv")
+    impressions = pd.read_csv("impressions.csv")
+
+    # On fusionne les 3 bases
+    fusion_1 = pd.merge(clics, impressions, on="cookie_id")
+    fusion = pd.merge(fusion_1, achats, on="cookie_id")
+
+    return jsonify(fusion)
+
+if __name__=='__API_NGUENANG__':
+ uvicorn.run(app, host='127.0.0.1',port=8000)
 
 
     
