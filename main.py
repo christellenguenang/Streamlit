b25f227c46dbf64b8a2a8c70952da42fedd8ce71
christellenguenang <108927688+christellenguenang@users.noreply.github.com>
2023-07-14 06:47:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -24,22 +24,21 @@ st.markdown(" ## I. Les Dashboards")
 #fusion = pd.DataFrame(fusion)
 
 # Importation des bases achats, clics et impressions
-
-    achats = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/achats.csv")
-    clics = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/clics.csv")
-    impressions = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/impressions.csv")
+achats = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/achats.csv")
+clics = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/clics.csv")
+impressions = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/impressions.csv")
 
     # On fusionne les 3 bases
-    fusion_1 = pd.merge(clics, impressions, on="cookie_id")
-    fusion = pd.merge(fusion_1, achats, on="cookie_id")
+fusion_1 = pd.merge(clics, impressions, on="cookie_id")
+fusion = pd.merge(fusion_1, achats, on="cookie_id")
     # changement du type des variables de temps
 
-    fusion['timestamp_x'] = pd.to_datetime(fusion['timestamp_x'], unit='s')
-    fusion['date_impressions'] = fusion['timestamp_x'].dt.strftime('01-01-1970 %H:%M:%S')
-    fusion['timestamp_y'] = pd.to_datetime(fusion['timestamp_y'], unit='s')
-    fusion['date_clics'] = fusion['timestamp_y'].dt.strftime('01-01-1970 %H:%M:%S')
-    fusion['timestamp'] = pd.to_datetime(fusion['timestamp'], unit='s')
-    fusion['date_achats'] = fusion['timestamp'].dt.strftime('01-01-1970 %H:%M:%S')
+fusion['timestamp_x'] = pd.to_datetime(fusion['timestamp_x'], unit='s')
+fusion['date_impressions'] = fusion['timestamp_x'].dt.strftime('01-01-1970 %H:%M:%S')
+fusion['timestamp_y'] = pd.to_datetime(fusion['timestamp_y'], unit='s')
+fusion['date_clics'] = fusion['timestamp_y'].dt.strftime('01-01-1970 %H:%M:%S')
+fusion['timestamp'] = pd.to_datetime(fusion['timestamp'], unit='s')
+fusion['date_achats'] = fusion['timestamp'].dt.strftime('01-01-1970 %H:%M:%S')
 
 
 
