bb80872050e183954c2ad48a628e3f600936bd7a
christellenguenang <108927688+christellenguenang@users.noreply.github.com>
2023-07-14 06:55:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # importation des librairies nécessaires
 from datetime import datetime
 import streamlit as st
-#from fastapi import FastAPI
+from fastapi import FastAPI
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import matplotlib.dates as mdates
@@ -9,38 +9,19 @@ import pandas as pd
 import seaborn as sns
 import httpx
 import numpy as np
-#app = FastAPI()
+app = FastAPI()
 
 
 st.title("Bienvenue sur mon application!")
 st.markdown(" ## I. Les Dashboards")
 
 
-# Importation des bases achats, clics et impressions
+# Importation des bases achats, clics et impression
 
-#with httpx.Client() as client:
- #   response = client.get("http://127.0.0.1:8000/fusion")
-#fusion= response.json()
-#fusion = pd.DataFrame(fusion)
-
-# Importation des bases achats, clics et impressions
-achats = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/achats.csv")
-clics = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/clics.csv")
-impressions = pd.read_csv("C:/Users/DELL/Desktop/ISE 2/Semestre 2/ML/Streamlit_NGUENANG/impressions.csv")
-
-    # On fusionne les 3 bases
-fusion_1 = pd.merge(clics, impressions, on="cookie_id")
-fusion = pd.merge(fusion_1, achats, on="cookie_id")
-    # changement du type des variables de temps
-
-fusion['timestamp_x'] = pd.to_datetime(fusion['timestamp_x'], unit='s')
-fusion['date_impressions'] = fusion['timestamp_x'].dt.strftime('01-01-1970 %H:%M:%S')
-fusion['timestamp_y'] = pd.to_datetime(fusion['timestamp_y'], unit='s')
-fusion['date_clics'] = fusion['timestamp_y'].dt.strftime('01-01-1970 %H:%M:%S')
-fusion['timestamp'] = pd.to_datetime(fusion['timestamp'], unit='s')
-fusion['date_achats'] = fusion['timestamp'].dt.strftime('01-01-1970 %H:%M:%S')
-
-
+with httpx.Client() as client:
+   response = client.get("http://127.0.0.1:8000/fusion")
+ fusion= response.json()
+fusion = pd.DataFrame(fusion)
 
 
 # Création des filtres
