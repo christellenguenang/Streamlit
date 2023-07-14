56cb53d4fb5bdaa039a74ada18548c5581da05e3
christellenguenang <108927688+christellenguenang@users.noreply.github.com>
2023-07-14 15:45:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ flask_url = "http://127.0.0.1:8000/fusion"  # Replace with your Flask server URL
 response = requests.get(flask_url)
 fusion_data = response.json()
 fusion = pd.DataFrame(fusion_data)
- fusion = fusion.replace("-", pd.NA)
+fusion = fusion.replace("-", pd.NA)
     
 # Changement du type des variables de temps
 
