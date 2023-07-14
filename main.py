f3654e49a3e5bc704a00ef934c7632d99df3f8ce
christellenguenang <108927688+christellenguenang@users.noreply.github.com>
2023-07-14 15:36:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__=='__main__':
 
 
     
-flask_url = "http://0.0.0.0:8000/fusion"  # Replace with your Flask server URL
+flask_url = "http://127.0.0.1:8000/fusion"  # Replace with your Flask server URL
 response = requests.get(flask_url)
 fusion_data = response.json()
 fusion = pd.DataFrame(fusion_data)
